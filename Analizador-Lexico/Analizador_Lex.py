7eb125e987f97072304eac63db5f6b72c7ddb9d6
Lucas Abad Bermejo <lucas.abad.bermejo@alumnos.upm.es>
2022-10-10 18:02:32 +0200

    Solucionado la lectura del archivo

--- Analizador-Lexico/Analizador_Lex.py
+++ Analizador-Lexico/Analizador_Lex.py
@@ -2,9 +2,15 @@
 
 
 def main():
-    archivo = open("Test/prueba.py", "r")
+    archivo = open("../Test/prueba.txt", "r")
     if not archivo.readable():
         return "Mensaje de error numero 1: Archivo no se puede leer"
     lineas = archivo.readlines()
     archivo.close()
+
+
+    pass
+
+if __name__ == "__main__" :
+    main()
 
