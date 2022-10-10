cf2c1c51d0d6cac9bf812a63bf71320987e10d70
Lucas Abad Bermejo <lucas.abad.bermejo@alumnos.upm.es>
2022-10-10 20:49:39 +0200

    Implementados todo los Tokens

--- Analizador-Lexico/Analizador_Lex.py
+++ Analizador-Lexico/Analizador_Lex.py
@@ -6,6 +6,8 @@ class AL:
     puntero = 0
     linea = 0
     cad = ""
+    num = 0
+    palres = ["if", "else", "function", "input", "return", "let", "print", "int", "string", "boolean"]
     file = "../Test/prueba.txt"
 
 def main():
@@ -24,28 +26,122 @@ def procesar_lineas(lineas):
     token = 0
     sigo = True
     while sigo:
+        #print(f"{len(lineas)}: {AL.linea} | {len(lineas[AL.linea])}: {AL.puntero} ")
         # String
         if lineas[AL.linea][AL.puntero] == '\"':
             AL.puntero += 1
             cadena(lineas[AL.linea])
             token = Tokens.Token.T("CAD", '\"'+AL.cad+'\"')
             AL.cad = ""
+        # Producto y Producto Asignado
+        elif lineas[AL.linea][AL.puntero] == '*':
+            AL.puntero += 1
+            if lineas[AL.linea][AL.puntero] == '=':
+                AL.puntero += 1
+                token = Tokens.Token.T("PrAsig")
+            else:
+                AL.puntero += 1
+                token = Tokens.TOken.T("Mult")
+
+        # Asignaccion
+        elif lineas[AL.linea][AL.puntero] == '=':
+            AL.puntero += 1
+            token = Tokens.Token.T("Asig")
+
+        # AND
+        elif lineas[AL.linea][AL.puntero] == '&':
+            AL.puntero += 1
+            if lineas[AL.linea][AL.puntero] == '&':
+                AL.puntero += 1
+                token = Tokens.Token.T("And")
+            else:
+                print("Error linea: {lineas[linea]}, se esperaba &")
+                err()
+        # Coma
+        elif lineas[AL.linea][AL.puntero] == ',':
+            AL.puntero += 1
+            token = Tokens.Token.T("Coma")
+        # Semicolon
+        elif lineas[AL.linea][AL.puntero] == ';':
+            AL.puntero += 1
+            token = Tokens.Token.T("SEMICOL")
+        # Parentesis Open
+        elif lineas[AL.linea][AL.puntero] == '(':
+            AL.puntero += 1
+            token = Tokens.Token.T("OpenPar")
+        # Parentesis Close
+        elif lineas[AL.linea][AL.puntero] == ')':
+            AL.puntero += 1
+            token = Tokens.Token.T("ClosePar")
+        # Llaves Open
+        elif lineas[AL.linea][AL.puntero] == '{':
+            AL.puntero += 1
+            token = Tokens.Token.T("OpenLlav")
+        # Llaves Close
+        elif lineas[AL.linea][AL.puntero] == '}':
+            AL.puntero += 1
+            token = Tokens.Token.T("CloseLlav")
+        # MayorQue
+        elif lineas[AL.linea][AL.puntero] == '>':
+            AL.puntero += 1
+            token = Tokens.Token.T("MayorQue")
+        # Init
+        elif digit(lineas[AL.linea][AL.puntero]):
+            AL.num = int(lineas[AL.linea][AL.puntero])
+            AL.puntero += 1
+            numeros(lineas[AL.linea])
+            if AL.num < 32768:
+                token = Tokens.Token.T("Entero", AL.num.__str__())
+            else:
+                print(f"Error en linea: {lineas[AL.linea]} el numero {AL.num} sobrepasas el limite")
+                err()
+            AL.num = 0
+        elif letra(lineas[AL.linea][AL.puntero]):
+            AL.cad = lineas[AL.linea][AL.puntero]
+            AL.puntero += 1
+            palabra(lineas[AL.linea])
+            if AL.cad.__eq__("true"):
+                token = Tokens.Token.T("Bool", AL.cad)
+            elif AL.cad.__eq__("false"):
+                token = Tokens.Token.T("Bool", AL.cad)
+            elif AL.cad in AL.palres:
+                token = Tokens.Token.T("Res", AL.cad)
+            else:
+                token = Tokens.Token.T("ID", AL.cad)
+            AL.cad = ""
+
         # Salto de linea
         elif lineas[AL.linea][AL.puntero] == '\n' or lineas[AL.linea][AL.puntero] == '\r':
             AL.linea += 1
             #EOF
             if AL.linea == len(lineas):
-                AL.puntero += 1
                 sigo = False
                 token = Tokens.Token.T("EOF")
             AL.puntero = 0
+        else:
+            AL.puntero += 1
         if token != 0:
             escribir(token)
             token = 0
 
     pass
 
+def digit (num):
+    return num.isnumeric()
 
+def numeros(linea):
+    if digit(linea[AL.puntero]):
+        AL.num = AL.num * 10 + int(linea[AL.puntero])
+        AL.puntero += 1
+        numeros(linea)
+def letra (letra):
+    return letra.isalpha()
+
+def palabra (linea):
+    if digit(linea[AL.puntero]) or letra(linea[AL.puntero]) or linea[AL.puntero] == '_':
+        AL.cad += linea[AL.puntero]
+        AL.puntero += 1
+        palabra(linea)
 def escribir(token):
     f = open("../Test/Archivos/Token.txt","a")
     f.write(token.__str__()+'\n')
